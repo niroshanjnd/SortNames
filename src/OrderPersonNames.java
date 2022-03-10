d081a47b5ee2d151ecd5a8d354c850be8820aec3
Janaka Dassanayake <niroshanjnd@gmail.com>
2022-03-11 00:21:06 +1000

    added logging and comments to the class

--- src/OrderPersonNames.java
+++ src/OrderPersonNames.java
@@ -1,10 +1,27 @@
 import java.io.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * <h1>Program to order person name by lastname and first name!</h1>
+ * The OrderPersonNames program reads data from a text file
+ * compare last names and first names
+ * and writes output to separate text file.
+ *
+ * @author Janaka Dassanayake
+ *
+ */
 public class OrderPersonNames {
 
     private static String fileName  = "inputNames";
+    private final static Logger LOGGER = Logger.getLogger(OrderPersonNames.class.getName());
 
+    /**
+     * This is the main method which makes use of displaySortedNames method.
+     * @param args Unused.
+     * @return Nothing.
+     */
     public static void main(String[] args) {
 
         File file = new File(fileName + ".txt");
@@ -12,37 +29,52 @@ public class OrderPersonNames {
 
         try {
             Scanner in = new Scanner(file);
-
             while (in.hasNext()) {
                 String lastName = in.next().trim();
                 String firstName = in.next().trim();
                 personArr.add(new Person(firstName, lastName));
             }
-
         } catch (FileNotFoundException e) {
+            LOGGER.setLevel(Level.SEVERE);
+            LOGGER.severe("Error Log starts here");
             e.printStackTrace();
         }
         displaySortedNames(personArr);
     }
 
+    /**
+     * This method is used to display ordered names.
+     * @param personArr person array
+     * @return nothing.
+     */
     private static void displaySortedNames(ArrayList<Person> personArr) {
         Collections.sort(personArr, new OrderNamesByLastAndFirst());
-
         File outputFile = new File(fileName + "-sorted.txt");
 
         try {
             if (outputFile.createNewFile()) {
-                System.out.println("File created: " + outputFile.getName());
+                LOGGER.setLevel(Level.INFO);
+                LOGGER.info("File created: " + outputFile.getName());
             } else {
-                System.out.println("File already exists.");
+                LOGGER.setLevel(Level.INFO);
+                LOGGER.info("File already exists.");
             }
             writeToOutputFile(personArr, outputFile);
-            System.out.println("Successfully wrote to the file.");
+            LOGGER.setLevel(Level.INFO);
+            LOGGER.info("Successfully wrote to the file.");
         } catch (IOException e) {
+            LOGGER.setLevel(Level.SEVERE);
+            LOGGER.severe("Error Log starts here");
             e.printStackTrace();
         }
     }
 
+    /**
+     * This method is used to write ordered names to the output file.
+     * @param personArr person array
+     * @param outputFile output file
+     * @return nothing.
+     */
     private static void writeToOutputFile(ArrayList<Person> personArr, File outputFile) throws IOException {
         FileOutputStream fos = new FileOutputStream(outputFile);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
